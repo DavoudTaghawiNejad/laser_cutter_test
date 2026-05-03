24d375398ca03589fbd7ea8c070aa50fda480b08
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2026-05-03 16:48:29 -0300

    Alternative origin calculation

---
+++ gcode/generate_test_gcode.py
@@ -0,0 +1,119 @@
+import yaml
+from types import SimpleNamespace
+import plac
+
+
+
+
+class Square:
+    def __init__(self, columns, rows, snippet_file, set_origin=True):
+        self.snippets = snippet_file
+        self.x = 0
+        self.y = 0
+        self.width = self.snippets.width
+        self.hight = self.snippets.hight
+        self.columns = columns
+        self.rows = rows
+        self.legend = [['' for x in range(columns)] for y in range(rows)]
+        self.finished = False
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *arg):
+        assert self.finished, 'finish_square not called'
+
+    def save_origin(self):
+        return 'G10 L2 P1 X0   Y0     ; G54  → column 1  (origin at 0 mm)'
+
+    def right(self, mm=None):
+        if mm is None:
+            mm=self.width
+        assert mm >= 0
+        self.x += mm
+        return self.snippets.move_origin_x.format(mm=-mm)
+
+    def left(self, mm=None):
+        if mm is None:
+            mm=self.width
+        assert mm >= 0
+        self.x -= mm
+        return self.snippets.move_origin_x.format(mm=mm)
+
+
+    def up(self, mm=None):
+        if mm is None:
+            mm=self.hight
+        assert mm >= 0
+        self.y += mm
+        return self.snippets.move_origin_y.format(mm=-mm)
+
+    def down(self, mm=None):
+        if mm is None:
+            mm=self.hight
+        assert mm >= 0
+        self.y -= mm
+        return self.snippets.move_origin_y.format(mm=mm)
+
+    def next_row(self):
+        distance = self.x
+        return self.up() + '\n' + self.left(mm=distance)
+
+    def back_to_origin(self):
+        self.x = 0
+        self.y = 0
+        return 'G54'
+
+    def position(self, column, row):
+        return self.snippets.temp_origin.format(x=self.width * column, y=self.hight * row)
+
+
+    def draw_object(self, power, speed, num_passes):
+        return '\n'.join([self.snippets.object.format(power=power, speed=speed) for _ in range(num_passes)])
+
+
+    def draw_at(self, column, row, power, speed, num_passes):
+        self.legend[row][column] = f'power: {int(power)}, speed: {int(speed)}, num_passes: {int(num_passes)}'
+        return self.position(column, row) + '\n' + self.draw_object(power, speed, num_passes)
+
+    def finish_square(self, power=20, speed=1500):
+        end = self.x // self.width
+        self.next_row()
+        self.finished = True
+        return '\n'.join([self.snippets.line_mm_right.format(power=power, speed=speed, mm=self.width / 2) + '\n' + self.right()
+                          for i in range(end)])
+
+
+
+@plac.pos('power_start',type=int)
+@plac.pos('power_stepsize',type=int)
+@plac.pos('power_steps',type=int)
+@plac.pos('speed_start',type=int)
+@plac.pos('speed_stepsize',type=int)
+@plac.pos('speed_steps',type=int)
+@plac.pos('min_passes',type=int)
+@plac.pos('max_passes',type=int)
+
+def generate(power_start:int, power_stepsize:int, power_steps:int, speed_start:int, speed_stepsize:int, speed_steps:int, min_passes:int, max_passes:int):
+    snippets = SimpleNamespace(**yaml.safe_load(open('snippets.yaml')))
+
+    gcode = [snippets.start]
+    for num_passes in range(min_passes, max_passes + 1):
+        with Square(columns=speed_steps, rows=power_steps, snippet_file=snippets) as square:
+            for row in range(power_steps):
+                power = power_start + row * power_stepsize
+                for column in range(10):
+                    speed = speed_start + column * speed_stepsize
+                    gcode.append(square.draw_at(column, row, power, speed, num_passes))
+            gcode.append(square.finish_square())
+    gcode.append(snippets.end)
+
+    open('output.gcode','w').write('\n'.join(gcode))
+
+
+
+
+
+
+if __name__ =="__main__":
+    plac.call(generate)
