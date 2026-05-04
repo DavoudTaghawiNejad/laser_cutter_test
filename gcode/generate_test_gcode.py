749e3a7879e95971d1a6e2c2dbc5752c8b32133c
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2026-05-03 21:09:00 -0300

    Can start new square for 3rd parameter, seperated by a line

--- gcode/generate_test_gcode.py
+++ gcode/generate_test_gcode.py
@@ -23,7 +23,7 @@ class Square:
         return self
 
     def __exit__(self, *arg):
-        assert self.finished, 'finish_square not called'
+        pass
 
     def right(self, mm=None):
         if mm is None:
@@ -55,10 +55,10 @@ class Square:
         return self.snippets.move_origin_y.format(mm=mm)
 
     def position(self, column, row):
-        undo = self.snippets.undo_origin.format(x=self.undo_x, y=self.undo_y)
+        undo = self.snippets.undo_set_origin.format(x=self.undo_x, y=self.undo_y)
         self.undo_x = self.width * column
         self.undo_y = self.hight * row
-        return undo + '\n' + self.snippets.temp_origin.format(x=self.width * column, y=self.hight * row)
+        return undo + '\n' + self.snippets.set_origin.format(x=self.width * column, y=self.hight * row)
 
 
     def draw_object(self, power, speed, num_passes):
@@ -69,14 +69,23 @@ class Square:
         self.legend[row][column] = f'power: {int(power)}, speed: {int(speed)}, num_passes: {int(num_passes)}'
         return self.position(column, row) + '\n' + self.draw_object(power, speed, num_passes)
 
-    def finish_square(self, power=20, speed=1500):
+    def hard_set_origin(self, x=None, y=None):
+        """ Sets the current origin to (x, y), if x or y is None current
+            position is set as origin """
+        if x is None:
+            x = self.undo_y
+        if y is None:
+            y = self.undo_y
+        self.undo_x = 0
+        self.undo_y = 0
+        return self.snippets.set_origin.format(x=x, y=y)
+
+    def new_square(self, power=20, speed=1500):
         end = self.x // self.width
-        self.next_row()
-        self.finished = True
-        return '\n'.join([self.snippets.line_mm_right.format(power=power, speed=speed, mm=self.width / 2) + '\n' + self.right()
+        left = self.position(0, self.undo_y / self.hight + 1)
+        line = '\n'.join([self.snippets.line_mm_right.format(power=power, speed=speed, mm=self.width / 2) + '\n' + self.right()
                           for i in range(end)])
-
-
+        return left + line + self.hard_set_origin()
 
 @plac.pos('power_start',type=int)
 @plac.pos('power_stepsize',type=int)
@@ -86,19 +95,18 @@ class Square:
 @plac.pos('speed_steps',type=int)
 @plac.pos('min_passes',type=int)
 @plac.pos('max_passes',type=int)
-
 def generate(power_start:int, power_stepsize:int, power_steps:int, speed_start:int, speed_stepsize:int, speed_steps:int, min_passes:int, max_passes:int):
     snippets = SimpleNamespace(**yaml.safe_load(open('snippets.yaml')))
 
     gcode = [snippets.start]
-    for num_passes in range(min_passes, max_passes + 1):
-        with Square(columns=speed_steps, rows=power_steps, snippet_file=snippets) as square:
+    with Square(columns=speed_steps, rows=power_steps, snippet_file=snippets) as square:
+        for num_passes in range(min_passes, max_passes + 1):
             for row in range(power_steps):
                 power = power_start + row * power_stepsize
                 for column in range(10):
                     speed = speed_start + column * speed_stepsize
                     gcode.append(square.draw_at(column, row, power, speed, num_passes))
-            gcode.append(square.finish_square())
+            gcode.append(square.new_square())
     gcode.append(snippets.end)
 
     open('output.gcode','w').write('\n'.join(gcode))
